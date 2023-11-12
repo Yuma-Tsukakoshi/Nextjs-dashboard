e78ce979f7c2099b9c6e3eef7b68f3ac4d48cf35
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-12 18:22:08 +0900

    add:Suspeceでコンポーネント表示

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -5,3 +5,5 @@ export default function Loading() {
 }
 // 特別なファイルシステム Loading 自動検知
 // (overview)フォルダを適応することで他のページにもスケルトンが反映されるのを防ぐ
+// ページ単位がloading.tsx 自動的にsuspenceをwrappingしてくれてる
+// suspenceは、コンポーネント単位でローディングを表示させる
