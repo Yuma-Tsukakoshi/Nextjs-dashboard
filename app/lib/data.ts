c03d3583e12c3c24e33980e4e92a7d6ca534efd9
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-14 16:01:31 +0900

    fix:invoice変更

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -154,7 +154,7 @@ export async function fetchInvoicesPages(query: string) {
 export async function fetchInvoiceById(id: string) {
   noStore();
   try {
-    const data = await sql<InvoiceForm>`
+    const data = await sql<InvoiceForm | undefined>`
       SELECT
         invoices.id,
         invoices.customer_id,
