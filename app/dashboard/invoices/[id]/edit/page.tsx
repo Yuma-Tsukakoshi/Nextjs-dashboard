eb1d12c768b866f649c03596dcac64fec0009b11
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-14 23:00:53 +0900

    add:error.tsxによるページ処理

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,9 +4,9 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  // Promise.all: to fetch both the invoice and customers in parallel:非同期で情報を取得する
+  // Promise.all: to fetch both the invoice and customers in parallel:非同期処理の並列実行
   const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
+    fetchInvoiceById(id), //初めの段階でidが渡されてないからinvoiceが定義されてない状態になってしまう
     fetchCustomers(),
   ]);
 
