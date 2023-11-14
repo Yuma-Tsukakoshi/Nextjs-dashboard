430b6bc600088fb410fb96c40a7918e282212229
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-14 23:09:45 +0900

    add:404エラーの実装

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import EditInvoiceForm from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -9,6 +10,10 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id), //初めの段階でidが渡されてないからinvoiceが定義されてない状態になってしまう
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
