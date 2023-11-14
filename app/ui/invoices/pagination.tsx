9750bc022393fd03bc17109c3e42d5b8f815b51b
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-11-14 11:39:43 +0900

    add:ページネーションレイアウト取得

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -9,10 +9,10 @@ import { usePathname, useSearchParams } from 'next/navigation';
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
 
-  // const allPages = generatePagination(currentPage, totalPages);
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
   
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
@@ -31,7 +31,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           isDisabled={currentPage <= 1}
         />
 
-        {/* <div className="flex -space-x-px">
+        <div className="flex -space-x-px">
           {allPages.map((page, index) => {
             let position: 'first' | 'last' | 'single' | 'middle' | undefined;
 
@@ -50,7 +50,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
               />
             );
           })}
-        </div> */}
+        </div>
 
         <PaginationArrow
           direction="right"
